82b1c0cc529aba1411b27dde81bc001cc3025e4b
bailuk <bailu@bailu.ch>
2021-09-08 19:30:53 +0200

    Add gir files for convenience / rename main class

---
+++ examples/src/main/java/examples/App.java
@@ -0,0 +1,19 @@
+package examples;
+
+import java.io.IOException;
+
+import ch.bailu.gtk.GTK;
+
+public class HelloWorld {
+
+    public static void main (String[] argv) throws IOException {
+        GTK.init();
+        // new HeaderBarSample(argv);
+        // new ColorChooser(argv);
+        // new ButtonBoxes(argv);
+        // new Spinner(argv);
+        // new LinksSample(argv);
+        //new CairoDrawingArea(argv);
+        new Pixbufs(argv);
+    }
+}
--- examples/src/main/java/examples/HelloWorld.java
+++ examples/src/main/java/examples/HelloWorld.java
@@ -1,19 +1,29 @@
 package examples;
 
-import java.io.IOException;
-
-import ch.bailu.gtk.GTK;
+import ch.bailu.gtk.gio.ApplicationFlags;
+import ch.bailu.gtk.gtk.Application;
+import ch.bailu.gtk.gtk.ApplicationWindow;
+import ch.bailu.gtk.gtk.Button;
 
 public class HelloWorld {
+    public HelloWorld(String[] argv) {
+        var app = new Application("com.example.GtkApplication",
+                ApplicationFlags.FLAGS_NONE);
 
-    public static void main (String[] argv) throws IOException {
-        GTK.init();
-        // new HeaderBarSample(argv);
-        // new ColorChooser(argv);
-        // new ButtonBoxes(argv);
-        // new Spinner(argv);
-        // new LinksSample(argv);
-        //new CairoDrawingArea(argv);
-        new Pixbufs(argv);
+        app.onActivate(() -> {
+            // Create a new window
+            var window = new ApplicationWindow(app);
+
+            // Create a new button
+            var button = Button.newWithLabelButton("Hello world!");
+
+            // When the button is clicked, close the window
+            button.onClicked(() -> window.close());
+            window.add(button);
+            window.showAll();
+        });
+
+        app.run(argv.length, argv);
     }
+
 }
