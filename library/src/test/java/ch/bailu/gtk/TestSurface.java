298a21bea742f03fe62ff9d17d03f92c5629b24e
bailuk <bailu@bailu.ch>
2021-09-15 19:50:10 +0200

    Test cairo surface

--- library/src/test/java/ch/bailu/gtk/TestSurface.java
+++ library/src/test/java/ch/bailu/gtk/TestSurface.java
@@ -1,2 +1,27 @@
-package ch.bailu.gtk;public class TestSurface {
+package ch.bailu.gtk;
+
+import org.junit.jupiter.api.Test;
+
+import ch.bailu.gtk.cairo.Cairo;
+import ch.bailu.gtk.cairo.Format;
+import ch.bailu.gtk.cairo.Surface;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSurface {
+    static {
+        System.loadLibrary("glue");
+    }
+
+    @Test
+    public void testSurface() {
+        Surface surface = Cairo.imageSurfaceCreate(Format.A8, 256,256);
+        assertNotEquals(0, surface.toLong());
+        surface.destroy();
+
+        byte[] bytes = new byte[256*256];
+        surface = Cairo.imageSurfaceCreateForData(bytes, Format.A8, 256, 256, 256);
+        assertNotEquals(0, surface.toLong());
+    }
+
 }
