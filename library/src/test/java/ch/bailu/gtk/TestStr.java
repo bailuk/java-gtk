52dece31c0882505ef21d299126f93b678c10f9e
bailuk <bailu@bailu.ch>
2021-10-02 22:11:35 +0200

    Add cairo functions and string test

---
+++ library/src/test/java/ch/bailu/gtk/TestStr.java
@@ -0,0 +1,24 @@
+package ch.bailu.gtk;
+
+import org.junit.jupiter.api.Test;
+
+import ch.bailu.gtk.wrapper.Str;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestStr {
+    static {
+        System.loadLibrary("glue");
+    }
+
+    @Test
+    public void testStr() {
+        Str s = new Str("test");
+
+        assertEquals("test", s.toString());
+        assertEquals("test".length()+1, s.getSize());
+        s.destroy();
+        assertEquals(0, s.getSize());
+    }
+
+}
