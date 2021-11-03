defdcc65b86b7d7a7803d68701b6881c45a6d4bd
bailuk <bailu@bailu.ch>
2021-11-03 17:12:57 +0100

    "is" is a reserved token in Kotlin

--- library/src/main/java/ch/bailu/gtk/GTK.java
+++ library/src/main/java/ch/bailu/gtk/GTK.java
@@ -23,10 +23,38 @@ public class GTK {
         }
     }
 
+    /**
+     * convert GTK boolean to Java boolean
+     * @param bool value to convert
+     * @return
+     */
+    public static boolean IS(int bool) {
+        return is(bool);
+    }
+
+    /**
+     * convert Java boolean to GTK boolean.
+     * @param bool value to convert
+     * @return
+     */
+    public static int IS(boolean bool) {
+        return is(bool);
+    }
+
+    /**
+     * convert GTK boolean to Java boolean
+     * @param bool value to convert
+     * @return
+     */
     public static boolean is(int bool) {
         return bool == TRUE;
     }
 
+    /**
+     * convert Java boolean to GTK boolean
+     * @param bool value to convert
+     * @return
+     */
     public static int is(boolean bool) {
         if (bool) {
             return TRUE;
