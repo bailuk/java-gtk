da4e572e3165619f026a89be3db6c7a7cad6e9b0
bailuk <bailu@bailu.ch>
2022-01-01 12:43:41 +0100

    Improve build scripts

--- ci/clone-and-build.sh
+++ ci/clone-and-build.sh
@@ -2,6 +2,6 @@
 
 test -d java-gtk || git clone "https://github.com/bailuk/java-gtk.git"
 cd java-gtk || exit 1
-git pull && git checkout gtk4
+git pull && git checkout main
 
 ./regenerateSources.sh && ./buildLibrary.sh $1 && ./reinstallToMavenLocal.sh && ./gradlew examples:build
--- regenerateSources.sh
+++ regenerateSources.sh
@@ -3,12 +3,15 @@
 # clean library module and glue module
 # compile and run source generator
 
-echo "\nClean all"
+echo
+echo "Clean all"
 ./gradlew -q library:clean || exit 1
 test -d glue/build && rm -r glue/build
 
-echo "\nGenerate sources"
+echo
+echo "Generate sources"
 ./gradlew -q generator:generate || exit 1
 
-echo "\nGenerate JNI headers"
+echo
+echo "Generate JNI headers"
 ./gradlew -q library:classes || exit 1
