d82fe8a95f688ea88ca245ba4293e852db3a6a50
bailuk <bailu@bailu.ch>
2022-01-08 13:19:46 +0100

    Symlinks are not supported

--- ci/debian/create.sh
+++ ci/debian/create.sh
@@ -3,14 +3,15 @@
 # https://wiki.debian.org/Packaging/Intro?action=show&redirect=IntroDebianPackaging
 version="0.1"
 
+test -d build && rm -rf build
 mkdir build
 cd ../../
 git archive --prefix=java-gtk-${version}/ --format=tar.gz  HEAD -o ci/debian/build/java-gtk_${version}.orig.tar.gz
 cd ci/debian/build
 tar -xzvf java-gtk_${version}.orig.tar.gz
 cd java-gtk-${version}
-ln -s ../../debian .
-cd debian
+cp -rv ../../debian .
+
 
 
 # export DEBEMAIL="bailu@bailu.ch"
@@ -18,3 +19,10 @@ cd debian
 
 # compatibility level for dephelper tool
 # echo 10 > debian/compat
+
+
+# build only source package
+# debuild -us -uc -S
+
+
+# debuild -us -uc -b
