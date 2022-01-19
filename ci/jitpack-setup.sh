84bc4b6377b92064336af9088544d6f9f6648a71
bailuk <bailu@bailu.ch>
2022-01-19 21:18:01 +0100

    Verify libraries

--- ci/jitpack-setup.sh
+++ ci/jitpack-setup.sh
@@ -2,16 +2,30 @@
 
 GLUE_DIR="java-gtk/build/resources/main/glue"
 
+cat /etc/os-release
+pwd
+
 test -f gradlew || cd ..
 
 VERSION="0.1"
 
 pwd
 
+if ! test -f build/pub-key.gpg; then
+  mkdir -p build
+  wget -nv -O - https://github.com/bailuk.gpg > build/pub-key
+  gpg --dearmor build/pub-key
+fi
+
+
 download_arch () {
-  mkdir -p ${GLUE_DIR}/${ARCH}
-  wget -nv --no-check-certificate -P ${GLUE_DIR}/${ARCH}/ https://bailu.ch/java-gtk/v${VERSION}/${ARCH}/libglue.so
-  wget -nv --no-check-certificate -P ${GLUE_DIR}/${ARCH}/ https://bailu.ch/java-gtk/v${VERSION}/${ARCH}/libglue.so.gpg
+  if ! test -d ${GLUE_DIR}/${ARCH}; then
+    mkdir -p ${GLUE_DIR}/${ARCH}
+    wget -nv --no-check-certificate -P ${GLUE_DIR}/${ARCH}/ https://bailu.ch/java-gtk/v${VERSION}/${ARCH}/libglue.so.gpg
+    gpg --keyring ./build/pub-key.gpg --no-default-keyring --verify ${GLUE_DIR}/${ARCH}/libglue.so.gpg || exit 1
+    gpg --output ${GLUE_DIR}/${ARCH}/libglue.so --decrypt ${GLUE_DIR}/${ARCH}/libglue.so.gpg || exit 1
+    rm ${GLUE_DIR}/${ARCH}/libglue.so.gpg
+  fi
 }
 
 ARCH="linux-aarch64"
@@ -20,5 +34,4 @@ download_arch
 ARCH="linux-x86_64"
 download_arch
 
-gpg --version
-cat /etc/os-release
+find -name "libglue*"
