4c9eacc55b760834ad1537d9280ba7c67bee9644
bailuk <bailu@bailu.ch>
2022-01-19 21:39:48 +0100

    Simplify verification

--- ci/jitpack-setup.sh
+++ ci/jitpack-setup.sh
@@ -22,8 +22,7 @@ download_arch () {
   if ! test -d ${GLUE_DIR}/${ARCH}; then
     mkdir -p ${GLUE_DIR}/${ARCH}
     wget -nv --no-check-certificate -P ${GLUE_DIR}/${ARCH}/ https://bailu.ch/java-gtk/v${VERSION}/${ARCH}/libglue.so.gpg
-    gpg --keyring ./build/pub-key.gpg --no-default-keyring --verify ${GLUE_DIR}/${ARCH}/libglue.so.gpg || exit 1
-    gpg --output ${GLUE_DIR}/${ARCH}/libglue.so --decrypt ${GLUE_DIR}/${ARCH}/libglue.so.gpg || exit 1
+    gpg --keyring ./build/pub-key.gpg --no-default-keyring -o ${GLUE_DIR}/${ARCH}/libglue.so -d ${GLUE_DIR}/${ARCH}/libglue.so.gpg || exit 1
     rm ${GLUE_DIR}/${ARCH}/libglue.so.gpg
   fi
 }
