4b7a2aa56fb6a891f65042db2578a27c2d613f54
bailuk <bailu@bailu.ch>
2024-02-13 20:54:00 +0100

    Add GTK tests

--- java-gtk/src/test/java/ch/bailu/gtk/TestPropertyAccess.java
+++ java-gtk/src/test/java/ch/bailu/gtk/TestPropertyAccess.java
@@ -5,11 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
 
+import ch.bailu.gtk.gdk.Display;
 import ch.bailu.gtk.gio.ListStore;
+import ch.bailu.gtk.gtk.Box;
+import ch.bailu.gtk.gtk.Button;
+import ch.bailu.gtk.gtk.Gtk;
+import ch.bailu.gtk.gtk.Orientation;
 import ch.bailu.gtk.gtk.TextTag;
+import ch.bailu.gtk.type.Str;
 
 public class TestPropertyAccess {
+    public static boolean gtkInit() {
+        return Gtk.initCheck() && Display.getDefault().isNotNull(); // Does not work inside Windows container
+    }
+
     @Test
     public void testPropertyAccess() {
         var listStore = new ListStore(TextTag.getTypeID());
@@ -27,5 +38,45 @@ public class TestPropertyAccess {
         assertFalse(textTag.getBooleanProperty("accumulative-margin"));
         textTag.setBooleanProperty("accumulative-margin", true);
         assertTrue(textTag.getBooleanProperty("accumulative-margin"));
+    }
+
+    @Test
+    @EnabledIf("gtkInit")
+    public void testPropertyAccessButton() {
+        var button = new Button();
+        var label = new Str("test2");
+
+        button.setStringProperty("label", "test");
+        assertEquals("test", button.getLabel().toString());
+        assertEquals("test", button.getStringProperty("label"));
+
+        button.setStrProperty("label", label);
+        assertEquals("test2", button.getLabel().toString());
+
+        assertTrue(button.getChild().isNotNull());
+        assertTrue(button.getObjectProperty("child").isNotNull());
+
+        assertEquals(button.getChild(), button.getObjectProperty("child"));
+    }
+
+
+    @Test
+    @EnabledIf("gtkInit")
+    public void testPropertyAccessBox() {
+        var box = new Box(Orientation.HORIZONTAL, 5);
+
+        int spacing = box.getIntProperty("spacing");
+        assertEquals(5, spacing);
+
+        box.setIntProperty("spacing", 10);
+        assertEquals(10, box.getSpacing());
+
+        box.setHomogeneous(false);
+        assertEquals(box.getHomogeneous(), box.getBooleanProperty("homogeneous"));
+        assertFalse(box.getHomogeneous());
+
+        box.setBooleanProperty("homogeneous", true);
+        assertEquals(box.getCanFocus(), box.getBooleanProperty("homogeneous"));
+        assertTrue(box.getHomogeneous());
     }
 }
